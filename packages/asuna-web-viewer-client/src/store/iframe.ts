ae0023bdd0e7f7d4027bf902325bd645d20f375d
unknown <linovys@gmail.com>
2022-03-18 20:02:22 -0500

    make pending block actions

--- packages/asuna-web-viewer-client/src/store/iframe.ts
+++ packages/asuna-web-viewer-client/src/store/iframe.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { MessageType, Payload_SC_SwapTexture } from 'asuna-web-live2d'
 import { sendMessage } from '../messenger'
+import { Dispatch, RootState } from './store'
 
 export enum IFrameStatus {
   Ready,
@@ -12,13 +13,18 @@ const initialState = {
   iFrameElement: null as (HTMLIFrameElement | null)
 }
 
-export const swapTexture = createAsyncThunk('iframe/send',
-  async () => {
+export const swapTexture = createAsyncThunk<void, void, { dispatch: Dispatch, state: RootState }>('iframe/send',
+  async (_, { dispatch, getState }) => {
+    if (getState().iframe.status === IFrameStatus.Pending) {
+      return
+    }
+    dispatch(setStatus(IFrameStatus.Pending))
     await sendMessage(MessageType.SC_SwapTexture, {
       modelId: 'model/000001',
       index: 0,
       variant: 1
     } as Payload_SC_SwapTexture)
+    dispatch(setStatus(IFrameStatus.Ready))
   }
 )
 
