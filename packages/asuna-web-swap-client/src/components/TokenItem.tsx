f9b3b9cccea31eecc77d9a34a20c4b987c08a6c2
unknown <linovys@gmail.com>
2022-03-31 18:18:55 -0500

    fix hover style

--- packages/asuna-web-swap-client/src/components/TokenItem.tsx
+++ packages/asuna-web-swap-client/src/components/TokenItem.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const TokenItem = function (props: React.PropsWithChildren<Props>) {
   const dispatch = useAppDispatch()
-  const selectedClass = props.selected ? ' bg-orange-600 hover:bg-orange-500' : ''
+  const selectedClass = props.selected ? ' bg-orange-600 hover:bg-orange-500' : ' bg-dark-600 hover:bg-slate-600'
 
   const handleClick = () => {
     dispatch(toggleSelected(props.tokenId))
@@ -17,7 +17,7 @@ const TokenItem = function (props: React.PropsWithChildren<Props>) {
 
   return (
     <div key={props.tokenId}
-      className={`overflow-hidden cursor-pointer rounded-md bg-dark-600 text-center hover:bg-slate-600${selectedClass}`}
+      className={`overflow-hidden cursor-pointer rounded-md text-center${selectedClass}`}
       onClick={handleClick}
     >
       {props.children}
