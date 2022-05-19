9733dac9d3e3423cacf8cc05780bc357d59d3248
unknown <linovys@gmail.com>
2022-05-19 18:13:02 -0500

    update to new contract

--- packages/asuna-web-regeneration-client/src/web3/HolderContract.ts
+++ packages/asuna-web-regeneration-client/src/web3/HolderContract.ts
@@ -1,4 +1,4 @@
-export const HOLDER_ADDRESS = '0xCe7031F5B2Eaaf7093b0634282ed48cdc7123a56'
+export const HOLDER_ADDRESS = '0xd5587825564f77ef9Fbbf5DF9D1Cc378eF23e230'
 export const HOLDER_ABI = [
   'function lock(uint16[] calldata tokenId) external',
   'function getLockedIds(address _address) external view returns(uint256[] memory)'
