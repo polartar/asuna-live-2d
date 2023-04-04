d269496697791a37a3d21a45631aa9efdcaab33f
unknown <linovys@gmail.com>
2023-04-04 06:39:23 -0500

    cleanup

--- packages/asuna-cubism-framework/src/legacy/live2dcubismframework.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Constant = void 0;
-exports.Constant = Object.freeze({
-    vertexOffset: 0,
-    vertexStep: 2 // Step value of the mesh vertex
-});
